6764a3d06dec4fed2614839f0c238640d071ae0b
Andrey Oskin <oskin.a.v@gmail.com>
2021-06-08 22:04:57 +1100

    add cli arg parser

---
+++ page_loader/arg_parser.py
@@ -0,0 +1,18 @@
+import argparse
+
+DESCRIPTION = r"""
+page_loader -f hexlet http://hexlet.io
+
+"""  # noqa: W291, W293
+
+
+def get_parser():
+    parser = argparse.ArgumentParser(
+        description="Utility to download web page from internet and save it locally.",  # noqa: E501
+        usage=DESCRIPTION,
+        formatter_class=argparse.RawTextHelpFormatter
+    )
+    parser.add_argument("url", help="Page address.")
+    parser.add_argument("-f", "--folder",
+                        help="Folder to save file.")
+    return parser
--- page_loader/scripts/page_loader.py
+++ page_loader/scripts/page_loader.py
@@ -1,10 +1,17 @@
 # flake8: noqa
 
 from page_loader import download
+from page_loader.arg_parser import get_parser
 
 
 def main():
-    download()
+    parser = get_parser()
+    args = parser.parse_args()
+    file_path = download(
+        args.url,
+        args.folder,
+    )
+    print(file_path)
 
 
 if __name__ == '__main__':
