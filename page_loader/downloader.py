0ea30f9e4b3e58bd2f78a16d0df83f5b44353357
Andrey Oskin <oskin.a.v@gmail.com>
2021-06-10 22:38:33 +1100

    add feature: image download refactor functions separately

--- page_loader/downloader.py
+++ page_loader/downloader.py
@@ -1,10 +1,12 @@
 import os
 import re
+from typing import Union, Optional
 
 import requests
+from bs4 import BeautifulSoup  # type: ignore
 
 
-def download(url: str, folder: str = None) -> str:
+def download(url: str, folder: Optional[str] = None) -> str:
     if folder is None:
         folder = os.getcwd()
     page = requests.get(url)
@@ -12,18 +14,55 @@ def download(url: str, folder: str = None) -> str:
         raise Exception(
             f'Error on page request. Page status returned: {page.status_code}',
         )
-
-    scheme = re.findall('.+//', url)[0]
-    trimmed_url = url[len(scheme):]
-    trimmed_url = trimmed_url[:-1] if trimmed_url[-1] == '/' else trimmed_url
-    ending = re.findall(r'\W\w+$', trimmed_url)[0]
-    if ending in {'.htm', '.html'}:
-        trimmed_url = trimmed_url[:-len(ending)]
-    transformed_url = re.sub(r'\W', '-', trimmed_url)
     if not os.path.isdir(folder):
         os.mkdir(folder)
-    file_name = f'{transformed_url}.html'
+    file_name = make_name(url, '.html')
     file_path = os.path.join(folder, file_name)
-    with open(file_path, 'w') as f:
+    with open(file_path, 'w') as f:  # noqa: WPS111
         f.write(page.text)
+    soup = BeautifulSoup(page.content)
+    img_folder = make_name(url, '_files')
+    img_folder = os.path.join(folder, img_folder)
+    for img in soup.find_all('img'):
+        img_src = img['src']
+        save_file(img_src, img_folder)
     return file_path
+
+
+def save_file(file_src: str, folder: str) -> None:
+    if not os.path.exists(folder):
+        os.mkdir(folder)
+    downloaded_file = requests.get(file_src)
+    file_name = make_name(file_src)
+    file_path = os.path.join(folder, file_name)
+    with open(file_path, 'wb') as f:  # noqa: WPS111
+        f.write(downloaded_file.content)
+
+
+def make_name(url: str, extension: Union[bool, str] = True) -> str:
+    """
+    Make file name out of url.
+
+    Name is made by first removing schema
+    and then replacing non-letters and non-numbers with '-'.
+
+    Add extension options:
+        True: default, keeps original extension
+        False: returns name without extension
+        str: adds specified extension
+
+    Example: https://example.com/file.jpg
+    Coverts to: example-com-file.jpg
+    """
+    scheme = re.findall('.+//', url)[0]
+    trimmed_url = url[len(scheme):]
+    trimmed_url = trimmed_url[1:] if trimmed_url[0] == '/' else trimmed_url
+    trimmed_url = trimmed_url[:-1] if trimmed_url[-1] == '/' else trimmed_url
+    ending = re.findall(r'\W\w+$', trimmed_url)[0]
+    trimmed_url = trimmed_url[:-len(ending)]
+    transformed_url = re.sub(r'\W', '-', trimmed_url)
+    if not extension:
+        return transformed_url
+    if extension is True:
+        return f'{transformed_url}{ending}'
+    return f'{transformed_url}{extension}'
