a0c9c7b4c0e85ffcb0c81fd79c9e9a9b58d4fc7d
MjH.Sakh <48555070+mjh-sakh@users.noreply.github.com>
2021-06-12 15:45:10 +1100

    delete old test

--- tests/test_implementation.py
+++ tests/test_implementation.py
@@ -1,9 +1,6 @@
-import os
-
 import pytest
 
-from tests.test_behavior import temp_folder
-from page_loader.downloader import is_absolute, save_file, make_name
+from page_loader.downloader import is_absolute, make_name
 
 
 @pytest.mark.parametrize("tested_link, result", [
@@ -14,14 +11,6 @@ from page_loader.downloader import is_absolute, save_file, make_name
 def test_is_absolute(tested_link, result):
     assert is_absolute(tested_link) == result
 
-
-@pytest.mark.parametrize("page_url, file_url", [
-    ("http://hpmor.com", "/snap-cover-small.jpg"),
-    ("doesnt matter", "http://www.hpmor.com/snap-cover-small.jpg")
-])
-def test_save_file(temp_folder, page_url, file_url):
-    save_file(file_url, page_url, 'img')
-    assert len(os.listdir('img')) == 1
 
 @pytest.mark.parametrize("raw, extension, result", [
     ('http://site.com', False, 'site-com'),
