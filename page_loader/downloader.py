fbf37ec63ec8efdd8e3cacf439f74a0a3f1306b1
Andrey Oskin <oskin.a.v@gmail.com>
2021-06-12 11:35:45 +1100

    fix work with relative links

--- page_loader/downloader.py
+++ page_loader/downloader.py
@@ -25,15 +25,23 @@ def download(url: str, folder: Optional[str] = None) -> str:
     img_folder = os.path.join(folder, img_folder)
     for img in soup.find_all('img'):
         img_src = img['src']
-        save_file(img_src, img_folder)
+        save_file(img_src, url, img_folder)
     return file_path
 
 
-def save_file(file_src: str, folder: str) -> None:
+def is_absolute(link: str) -> bool:
+    return True if re.search('//', link) else False
+
+
+def save_file(file_url: str, page_url: str, folder: str) -> None:
     if not os.path.exists(folder):
         os.mkdir(folder)
-    downloaded_file = requests.get(file_src)
-    file_name = make_name(file_src)
+    if is_absolute(file_url):
+        absolute_file_url = file_url
+    else:
+        absolute_file_url = fr'{page_url}/{file_url}'
+    downloaded_file = requests.get(absolute_file_url)
+    file_name = make_name(file_url)
     file_path = os.path.join(folder, file_name)
     with open(file_path, 'wb') as f:  # noqa: WPS111
         f.write(downloaded_file.content)
