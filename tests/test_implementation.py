34626d3cae97cd2133fa84f323f395113f189c31
Andrey Oskin <oskin.a.v@gmail.com>
2021-06-12 13:49:08 +1100

    add test for make name

--- tests/test_implementation.py
+++ tests/test_implementation.py
@@ -3,7 +3,7 @@ import os
 import pytest
 
 from tests.test_behavior import temp_folder
-from page_loader.downloader import is_absolute, save_file
+from page_loader.downloader import is_absolute, save_file, make_name
 
 
 @pytest.mark.parametrize("tested_link, result", [
@@ -22,3 +22,23 @@ def test_is_absolute(tested_link, result):
 def test_save_file(temp_folder, page_url, file_url):
     save_file(file_url, page_url, 'img')
     assert len(os.listdir('img')) == 1
+
+@pytest.mark.parametrize("raw, extension, result", [
+    ('http://site.com', False, 'site-com'),
+    ('http://site.com', True, 'site-com'),
+    ('http://site.com', '.text', 'site-com.text'),
+    ('http://web.site.ru/here', False, 'web-site-ru-here'),
+    ('http://web.site.ru/here', True, 'web-site-ru-here'),
+    ('http://web.site.ru/here', '.text', 'web-site-ru-here.text'),
+    ('http://web.site.ru/here.ext', False, 'web-site-ru-here'),
+    ('http://web.site.ru/here.ext', True, 'web-site-ru-here.ext'),
+    ('http://web.site.ru/here.ext', '.text', 'web-site-ru-here.text'),
+    ('site', False, 'site'),
+    ('site', True, 'site'),
+    ('site', '.text', 'site.text'),
+    ('site/one.ext', False, 'site-one'),
+    ('site/one.ext', True, 'site-one.ext'),
+    ('site/one.ext', '.text', 'site-one.text'),
+])
+def test_make_name(raw, extension, result):
+    assert make_name(raw, extension) == result
