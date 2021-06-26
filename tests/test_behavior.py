6bc847144738485c3f0d5e326212ddc0c0e6c0e5
MjH.Sakh <48555070+mjh-sakh@users.noreply.github.com>
2021-06-26 18:36:43 +1100

    refactor to use requests_mock

--- tests/test_behavior.py
+++ tests/test_behavior.py
@@ -5,6 +5,9 @@ from pathlib import Path
 from typing import TextIO
 
 import pytest
+from bs4 import BeautifulSoup as bs
+from requests import exceptions
+from requests_mock import ANY as mock_ANY
 
 from page_loader import download
 from page_loader.downloader import SYSTEM_EXIT_CODES
@@ -19,15 +22,11 @@ def locate(file_name):
     return file_path
 
 
-def check_if_same(file1: TextIO, file2: TextIO):
-    lines1 = file1.readlines()
-    lines2 = file2.readlines()
-    if len(lines1) != len(lines2):
-        return False
-    for line1, line2 in zip(lines1, lines2):
-        if line1 != line2:
-            return False
-    return True
+def read_text(file: str):
+    text: str
+    with open(file, encoding='utf-8') as f:
+        text = f.read()
+    return text
 
 
 @pytest.fixture
@@ -41,7 +40,8 @@ def temp_folder():
 @pytest.mark.parametrize("page_url, expected_file", [
     ("https://sheldonbrown.com/harris/bikes.html", "sheldonbrown-com-harris-bikes.html"),
 ])
-def test_download(temp_folder, page_url, expected_file):
+def test_download(temp_folder, page_url, expected_file, requests_mock):
+    requests_mock.get(page_url, text=read_text(locate(expected_file)))
     file_path = download(page_url, temp_folder)
     assert os.path.isfile(file_path)
     folder_path, file_name = os.path.split(file_path)
@@ -64,19 +64,21 @@ def test_download(temp_folder, page_url, expected_file):
 @pytest.mark.parametrize("page_url", [
     ("https://sheldonbrown.com/harris/bikes.html"),
 ])
-def test_download_defaults_to_cwd(temp_folder, page_url):
+def test_download_defaults_to_cwd(temp_folder, page_url, requests_mock):
+    requests_mock.get(page_url, text="test")
     file_path = download(page_url)
     folder_path = os.path.dirname(file_path)
     assert Path(folder_path).resolve() == Path(temp_folder).resolve()
 
 
 @pytest.mark.parametrize("page_url, core_name, expected_names", [
-    ("https://sheldonbrown.com/harris/bikes.html", "sheldonbrown-com-harris-bikes",
-     "sheldonbrown-com-harris-bikes-files.txt"),  # absolute links
-    ("http://hpmor.com", "hpmor-com", "hpmor-com-files.txt"),  # relative links
+    # ("https://sheldonbrown.com/harris/bikes.html", "sheldonbrown-com-harris-bikes", "sheldonbrown-com-harris-bikes-files.txt"),  # absolute links
+    # ("http://hpmor.com", "hpmor-com", "hpmor-com-files.txt"),  # relative links
+    ('http://hexlet.io/courses', 'hexlet-io-courses', 'hexlet-io-courses-files.txt')
 ])
-def test_download_saves_imgs(temp_folder, page_url, core_name, expected_names, caplog):
-    # caplog.set_level(logging.DEBUG)
+def test_download_saves_imgs(temp_folder, page_url, core_name, expected_names, caplog, requests_mock):
+    requests_mock.get(url=mock_ANY, text="test")  # for src
+    requests_mock.get(page_url, text=read_text(locate(f'original_{core_name}.html')))  # for page
     subfolder = 'subfolder'
     os.mkdir(subfolder)
     file_path = download(page_url, subfolder)
@@ -89,27 +91,30 @@ def test_download_saves_imgs(temp_folder, page_url, core_name, expected_names, c
     assert sorted(saved_names_list) == sorted(expected_names_list)
 
 
-@pytest.mark.parametrize("url, folder, expected_ex_type, expected_sys_exit_code", [
-    ('abracadabra', None, ConnectionError, SYSTEM_EXIT_CODES['connection_bad_url']),
-    ('ya.ru/abracadabra', None, ConnectionError, SYSTEM_EXIT_CODES['connection_bad_response']),
-    ('httppp://ya.ru/abracadabra', None, ConnectionError, SYSTEM_EXIT_CODES['connection_other']),
-    ('http://hexlet.io', 'test', FileNotFoundError, SYSTEM_EXIT_CODES['file_not_found']),
+@pytest.mark.parametrize("url, folder, expected_ex_type, expected_sys_exit_code, mock_kwargs", [
+    ('abracadabra', None, ConnectionError, SYSTEM_EXIT_CODES['connection_bad_url'], {'url': 'http://abracadabra/', 'exc': exceptions.ConnectionError}),
+    ('ya.ru/abracadabra', None, ConnectionError, SYSTEM_EXIT_CODES['connection_bad_response'], {'url': 'http://ya.ru/abracadabra', 'status_code': 404}),
+    ('httppp://ya.ru/abracadabra', None, ConnectionError, SYSTEM_EXIT_CODES['connection_other'], {'url': 'httppp://ya.ru/abracadabra', 'exc': exceptions.InvalidSchema}),
+    ('http://hexlet.io', 'test', FileNotFoundError, SYSTEM_EXIT_CODES['file_not_found'], {'url': 'http://hexlet.io', 'text': 'test'}),
 ])
-def test_download_exit_codes(temp_folder, url, folder, expected_ex_type, expected_sys_exit_code, caplog):
+def test_download_exit_codes(temp_folder, url, folder, expected_ex_type, expected_sys_exit_code, mock_kwargs, caplog, requests_mock):
     caplog.set_level(logging.DEBUG)
+    requests_mock.get(**mock_kwargs)
     with pytest.raises(expected_ex_type) as ex_info:
         download(url, folder)
     assert ex_info.value.args[-1] == expected_sys_exit_code
 
 
-@pytest.mark.parametrize("url, folder, expected_log_message", [
-    ('abracadabra', None, 'Invalid url'),
-    ('httppp://ya.ru/abracadabra', None, 'Some other error arose'),
-    ('ya.ru', None, 'Looks that schema is missed'),
-    ('http://ya.ru/abracadabra', None, 'Was not able to load page. Aborted.'),
-    ('http://hexlet.io', 'test', "Folder doesn't exist:"),
+@pytest.mark.parametrize("url, folder, expected_log_message, mock_kwargs", [
+    ('ya.ru', None, 'Looks that schema is missed', {'url': 'http://ya.ru', 'text': 'test'}),
+    ('http://hexlet.io', 'test', "Folder doesn't exist:", {'url': 'http://hexlet.io', 'text': 'test'}),
+    ('http://ya.ru/abracadabra', None, 'Was not able to load page. Aborted.', {'url': 'http://ya.ru/abracadabra', 'status_code': 404}),
+    ('httppp://ya.ru/abracadabra', None, 'Some other error arose', {'url': 'httppp://ya.ru/abracadabra', 'exc': exceptions.InvalidSchema}),
+    ('abracadabra', None, 'Invalid url', {'url': 'http://abracadabra/', 'exc': exceptions.ConnectionError}),
+    ('ya.ru', None, 'Some other error arose', {'url': 'http://ya.ru', 'exc': exceptions.Timeout}),
 ])
-def test_download_writes_log(temp_folder, url, folder, expected_log_message, caplog):
+def test_download_writes_log(temp_folder, url, folder, expected_log_message, mock_kwargs, caplog, requests_mock):
+    requests_mock.get(**mock_kwargs)
     caplog.set_level(logging.DEBUG)
     try:
         download(url, folder)
