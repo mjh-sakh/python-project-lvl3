e32b16aa2616d97f0ab99d5f91389386985cf8b6
mjh-sakh <madjahead@gmail.com>
2021-07-20 09:05:41 +0300

    refactor to simplify exit codes

--- page_loader/__init__.py
+++ page_loader/__init__.py
@@ -4,11 +4,9 @@ __version__ = "0.1.0"
 from types import MappingProxyType
 
 SYSTEM_EXIT_CODES = MappingProxyType({
-    'connection_bad_url': 10,
-    'connection_bad_response': 11,
-    'connection_other': 19,
-    'file_not_found': 20,
-    'file_permission': 21,
+    'connection_err': 10,
+    'file_sys_err': 11,
+    'other': 2,
 })
 
 from page_loader.downloader import download
